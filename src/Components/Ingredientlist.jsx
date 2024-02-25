e1ec06e36a91ae1045b38f252d0953442094c127
olcayprojects <olcaydeveloper@gmail.com>
2024-02-25 07:56:19 +0300

    add checkbox

--- src/Components/Ingredientlist.jsx
+++ src/Components/Ingredientlist.jsx
@@ -5,41 +5,53 @@ export default function Ingredientlist(props) {
   let ingredientURL = "https://www.themealdb.com/images/ingredients/";
 
   return (
-    <ol className="list-group list-group-numbered fs-5 point">
+    <ul className="list-group fs-5 point">
       <h5 className="text-center text-secondary fw-bold">
         {" "}
-        <span className="bg-dark text-warning border border-warning px-2">
-          Ingredients
+        <span className=" text-warning">
+          The ingredients for {props.item.strMeal}!
         </span>
       </h5>
       {Object.keys(props.item).map((items, index) => {
         return props.item["strIngredient" + index] ? (
-          <li
-            id={props.item["strIngredient" + index]}
-            title={"Click go to " + props.item["strIngredient" + index] + " Recipes"}
-            key={index}
-            onClick={(ef) => {
-              navigate(
-                `/ingredientsfilter/${props.item["strIngredient" + index]}`
-              );
-            }}
-            className="list-group-item list-group-item-action bg-dark text-warning border-warning border border-2 p-2"
-          >
-            <span>{props.item["strMeasure" + index].trim() + " "}</span>
-            <img
-              className="img-fluid"
-              style={{ width: "25px" }}
-              src={
-                ingredientURL +
+          <div className="" key={index}>
+            <input
+              className="form-check-input me-3"
+              type="checkbox"
+              value=""
+              aria-label="..."
+            ></input>
+            <li
+              id={props.item["strIngredient" + index]}
+              title={
+                "Click go to " +
                 props.item["strIngredient" + index] +
-                "-Small.png"
+                " Recipes"
               }
-              alt=""
-            />
-            <span>{" " + props.item["strIngredient" + index]}</span>
-          </li>
+              key={index}
+              onClick={(ef) => {
+                navigate(
+                  `/ingredientsfilter/${props.item["strIngredient" + index]}`
+                );
+              }}
+              className="list-group-item-action bg-dark text-warning border-warning border-bottom p-0 d-inline"
+            >
+              <span>{props.item["strMeasure" + index].trim() + " "}</span>
+              <img
+                className="img-fluid"
+                style={{ width: "25px" }}
+                src={
+                  ingredientURL +
+                  props.item["strIngredient" + index] +
+                  "-Small.png"
+                }
+                alt=""
+              />
+              <span className="fw-bold">{" " + props.item["strIngredient" + index]}</span>
+            </li>
+          </div>
         ) : null;
       })}
-    </ol>
+    </ul>
   );
 }
