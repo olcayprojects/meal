d434d84c84d78475b0cb955b2376efbd00f473d2
olcayprojects <olcaydeveloper@gmail.com>
2025-02-25 09:25:50 +0300

    text add

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,7 +1,15 @@
 import React, { useState, useEffect } from "react";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const currentDate = new Date();
+  const year = currentDate.getFullYear(); // Yıl bilgisini al
+
+  return (
+    <div className="text-center text-warning-emphasis">
+      &copy; {year} Explore a variety of delicious recipes and make every meal
+      special. Find easy and tasty recipes for every occasion.
+    </div>
+  );
 };
 
 export default Footer;
