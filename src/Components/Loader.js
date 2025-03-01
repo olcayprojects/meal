630cd6ef8b55688736ede3686e95f1f5f9c7af40
olcayprojects <olcaydeveloper@gmail.com>
2025-03-01 13:27:07 +0300

    design

--- src/Components/Loader.js
+++ src/Components/Loader.js
@@ -15,11 +15,11 @@ function Loader() {
   return (
     <div className="d-flex flex-grow-1 justify-content-center align-items-center">
       <div
-        class="spinner-grow text-warning"
+        className="spinner-grow text-warning"
         style={{ width: "300px", height: "300px" }}
         role="status"
       >
-        <span class="visually-hidden">Loading...</span>
+        <span className="visually-hidden">Loading...</span>
       </div>
     </div>
   );
